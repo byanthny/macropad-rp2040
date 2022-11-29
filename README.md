ed8b59bf0f29306226d607a9ebf231edbb9df5ff
Anthony Das <anthonydas46@gmail.com>
2022-11-28 20:13:14 -0500

    init: created project

---
+++ README.md
@@ -0,0 +1,41 @@
+# RP2040 Macropad
+
+my code (and future libraries) for custom macropad built with adafruit macropad rp2040 kit
+
+## Config
+
+Follow [adafruit tutorial](https://learn.adafruit.com/adafruit-macropad-rp2040) for adafruit macropad rp2040 kit with CircuitPython
+
+Copy `code.py` and `/lib` to root of macropad, see tutorial for more info.
+
+## Features
+
+## Rotary Encoder
+
+- Encoder: Volume Control
+- Switch: Pause/Play
+
+## Keys
+
+0. Discord Mute (Custom keybind, bug with discord repeating keys)
+1. None
+2. Toggle between brightness modes (to expand later)
+
+## TODO (no order)
+
+- [ ] Build with rust
+- [ ] Map out rest of keys
+- [ ] Add more light modes
+  - [ ] RGB
+- [ ] Create profiles
+- [ ] Use OLED more
+- [ ] Check out [Macropad Hotkeys Project Guide](https://learn.adafruit.com/macropad-hotkeys) guide
+
+# Resources
+
+- [Adafruit Macropad RP2040 Guide](https://learn.adafruit.com/adafruit-macropad-rp2040)
+- [CircuitPython USB HID Driver Docs](https://docs.circuitpython.org/projects/hid/en/latest/index.html)
+- [Adafruit MacroPad RP2040 Helper Library Docs](https://docs.circuitpython.org/projects/macropad/en/latest/index.html)
+- [CircuitPython Docs](https://docs.circuitpython.org/en/latest/README.html)
+- [Macropad Hotkeys Project Guide](https://learn.adafruit.com/macropad-hotkeys)
+- [RP2040 MacOS Ventura Issue](https://www.raspberrypi.com/news/the-ventura-problem/)
---
+++ src/code.py
@@ -0,0 +1,70 @@
+from adafruit_macropad import MacroPad
+import time
+
+macropad = MacroPad()
+
+text_lines = macropad.display_text(title="behold!")
+key_commands = ["Toggle Discord Mute", "", "Change Light Mode", "", "", "", "", "", "", "", "", ""]
+num_modes = 4
+
+mode = 1
+last_position = 0
+
+while True:
+    key_event = macropad.keys.events.get()
+    
+    text_lines[0].text=" "
+    text_lines[1].text=" "
+
+    if key_event:
+        if key_event.pressed:
+            
+            text_lines[1].text=key_commands[key_event.key_number]
+            macropad.pixels[key_event.key_number] = (52, 199, 220)
+
+            # Discord Mute
+            if key_event.key_number == 0:
+                macropad.keyboard.press(
+                    macropad.Keycode.COMMAND, macropad.Keycode.SHIFT, macropad.Keycode.L, macropad.Keycode.M
+                )
+                macropad.keyboard.release_all()
+
+            # Change Light Mode
+            if key_event.key_number == 2:
+                mode = mode+1 if mode < 4 else 1
+                if mode == 1:
+                    macropad.pixels.brightness = 0.5
+                if mode == 2:
+                    macropad.pixels.brightness = 0.25
+                if mode == 3:
+                    macropad.pixels.brightness = 0.05
+                if mode == 4:
+                    macropad.pixels.brightness = 0.0
+            # TODO RGB MODES and stuff
+
+        time.sleep(0.4)
+
+    macropad.encoder_switch_debounced.update()
+
+    if macropad.encoder_switch_debounced.pressed:
+        macropad.consumer_control.send(
+                macropad.ConsumerControlCode.PLAY_PAUSE
+        )
+
+    current_position = macropad.encoder
+
+    if macropad.encoder > last_position:
+        macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_INCREMENT
+        )
+        last_position = current_position
+
+    if macropad.encoder < last_position:
+        macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_DECREMENT
+        )
+        last_position = current_position
+    
+    macropad.pixels.fill((219, 123, 48))
+
+    text_lines.show()
