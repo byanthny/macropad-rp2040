bafb31fb5d8c8d7fd1b7a99d08861bfba0a57282
Anthony Das <dev@anthnydas.com>
2025-07-04 16:55:15 -0400

    basic entry & setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+//import panic handler (other options are panic_abort, panic_reset, ...)
+//"as _" imported/linked w/o name
+use panic_halt as _; 
+
+// Import the entry macro from cortex-m runtime
+//This macro knows how to properly set up an ARM Cortex-M program.
+use cortex_m_rt::entry;
+
+// Import the bootloader for RP2040
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+// main loop & ! means this function never returns
+//#[no_mangle no mangling (changing) of the function name
+#[entry] 
+fn main() -> ! {
+    loop {
+        //forever
+    }
 }
