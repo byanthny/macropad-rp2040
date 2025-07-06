8e089865a16adc465aab243bc4cc2509dc1ed5dd
Anthony Das <dev@anthnydas.com>
2025-07-05 21:52:36 -0400

    runner setup and failed blink

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# RP2040 Macropad [use at your own risk]
+# RP2040 Macropad w/ Rust (Adafruit Kit) [use at your own risk]
 
-my code (and future libraries) for custom macropad built with adafruit macropad rp2040 kit
+my code (and future libraries) for custom macropad built with adafruit macropad rp2040 kit and rust.
 
 ## Config
 
-Follow [adafruit tutorial](https://learn.adafruit.com/adafruit-macropad-rp2040) for adafruit macropad rp2040 kit with CircuitPython
-
-Copy `code.py` and `/lib` to root of macropad, see tutorial for more info.
+coming soon
 
 ## Features
 
@@ -25,28 +23,16 @@ Copy `code.py` and `/lib` to root of macropad, see tutorial for more info.
 
 ## TODO (no order)
 
-- [ ] Build with rust?
-- [ ] Map out rest of keys
-- [ ] Add more light modes
-  - [ ] RGB
-- [ ] Create profiles & switching
-- [ ] Use OLED more
-- [ ] Check out [Macropad Hotkeys Project Guide](https://learn.adafruit.com/macropad-hotkeys) guide
-- [ ] Refactor message system and macros system to be more modular.
-- [ ] double press and combination macros?
-- [ ] keys & actions on oled
+- [ ] Rust
 
 ## Bugs
-
-- [x] Remove sleep causing unresponsiveness but maintain message duration
-- [ ] Random reloads?
 
 # Resources
 
 - [Adafruit Macropad RP2040 Guide](https://learn.adafruit.com/adafruit-macropad-rp2040)
-- [CircuitPython USB HID Driver Docs](https://docs.circuitpython.org/projects/hid/en/latest/index.html)
 - [Adafruit MacroPad RP2040 Helper Library Docs](https://docs.circuitpython.org/projects/macropad/en/latest/index.html)
-- [CircuitPython Docs](https://docs.circuitpython.org/en/latest/README.html)
 - [Macropad Hotkeys Project Guide](https://learn.adafruit.com/macropad-hotkeys)
 - [RP2040 MacOS Ventura Issue](https://www.raspberrypi.com/news/the-ventura-problem/)
-- [Datasheet](https://github.com/adafruit/Adafruit-MacroPad-RP2040-PCB/blob/fdd7f2cb3bc2b3c7a9c0765780387647ea872141/Adafruit%20MacroPad%20RP2040%20Pinout.pdf)
+- [Adafruit MacroPad Datasheet](https://github.com/adafruit/Adafruit-MacroPad-RP2040-PCB/blob/fdd7f2cb3bc2b3c7a9c0765780387647ea872141/Adafruit%20MacroPad%20RP2040%20Pinout.pdf)
+- [RP2040 HAL Template (pins are different used for setup)](https://github.com/rp-rs/rp2040-project-template)
+- [](https://lib.rs/crates/adafruit-macropad)
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,10 @@ use cortex_m_rt::entry;
 
 // hardware access
 use rp2040_hal as hal;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::OutputPin;
 use hal::{
     clocks::{init_clocks_and_plls, Clock}, //clock configurations
+    gpio,  // GPIO pin access
     pac,  // Peripheral Access Crate, "raw hardware access"
     sio::Sio,  // GPIO
     watchdog::Watchdog,
@@ -29,7 +30,7 @@ pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H; // import t
 //#[no_mangle no mangling (changing) of the function name
 #[entry] 
 fn main() -> ! {
-
+    //info!("Program start");
     // take() takes ownership and unwrap() unwraps the result, panicking if in use already
     let mut pac = pac::Peripherals::take().unwrap(); // take ownership of the peripherals (RP2040 hardware)
     let core = cortex_m::Peripherals::take().unwrap(); // take ownership of the core peripherals (Cortex-M hardware)
@@ -40,7 +41,7 @@ fn main() -> ! {
 
     // Setup clocks and PLLs (Phase-Locked Loops)
     let clocks = init_clocks_and_plls(
-        12_000_000u32,  // 12MHz crystal frequency - ADD THIS LINE
+        12_000_000u32, // 12 MHz external crystal frequency
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -55,22 +56,21 @@ fn main() -> ! {
 
     // GPIO pin setup
     let sio = Sio::new(pac.SIO); // single-cycle IO (fastest refresh of pin states in 1 clock cycle)
-    let pins = hal::gpio::Pins::new(
+    let pins = gpio::Pins::new(
         pac.IO_BANK0, // pins for config
         pac.PADS_BANK0, // rest of the pins
         sio.gpio_bank0, // SIO (RP2040 feature)
         &mut pac.RESETS, // "mutable reference to the reset controller"
     );
 
-    // Configure GPIO pin 1 as a push-pull output (for an LED)
-    // Pushes current to the pin when high, pulls it to 0V
-    let mut led_pin = pins.gpio1.into_push_pull_output();
+    // Configure the built-in LED pin as output
+    let mut led_pin = pins.gpio13.into_push_pull_output();
 
     // Flashes the LED on and off every 500ms
     loop {
-        led_pin.set_high().unwrap();  // Set High, 3.3V (LED on)
-        delay.delay_ms(500);          // Wait 500ms using the delay object
-        led_pin.set_low().unwrap();   // Set Low, 0V (LED off)  
-        delay.delay_ms(500);    
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
     }
 }
